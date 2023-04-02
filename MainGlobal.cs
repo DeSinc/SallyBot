5571a6212ca8b977c3530291a575654abf4bce46
DeSinc <6deans@gmail.com>
2023-04-03 02:06:22 +1000

    Update MainGlobal.cs

--- MainGlobal.cs
+++ MainGlobal.cs
@@ -9,7 +9,7 @@ namespace SallyBot
         internal static SocketGuild Server { get; set; }
         internal static DiscordSocketClient Client { get; set; }
         
-        internal static int ClientHash { get; set; }
+        internal static int ClientHash { get; set; } // you can use this to detect which idk I forgot mid sentence while I was typing why I put this here I think I use it to validate client's session so a subsequent run of the program can't access the db while the prior one is still going? idk
         
         internal static string conS = "BOT_TOKEN_HERE";
     }
