2577b2ecdb2e5d424cbea2831dd5474557bef894
DeSinc <6deans@gmail.com>
2023-04-06 18:58:04 +1000

    Update MainGlobal.cs

--- MainGlobal.cs
+++ MainGlobal.cs
@@ -9,6 +9,6 @@ namespace SallyBot
         internal static SocketGuild Server { get; set; }
         internal static DiscordSocketClient Client { get; set; }
 
-        internal static string conS = "NDM4NjM0OTc5ODYyNTExNjE2.DcHlNA.RNY1yEpfc6jZPnH3ARhoVEn8uEo";
+        internal static string conS = "BOT_TOKEN_HERE";
     }
 }
