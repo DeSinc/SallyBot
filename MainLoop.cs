763d390743eb67022a859116e0b3663a5f9de220
SlenderFox <36467674+SlenderFox@users.noreply.github.com>
2023-04-09 21:42:06 +1000

    Moved guild ID to MainGlobal

--- MainLoop.cs
+++ MainLoop.cs
@@ -7,12 +7,12 @@ namespace SallyBot
     {
         internal static Task StartLoop()
         {
-            MainGlobal.Server = MainGlobal.Client.GetGuild(PUT_YOUR_SERVER_ID_HERE_OTHERWISE_KNOWN_AS_GUILD_ID);
+            MainGlobal.Server = MainGlobal.Client.GetGuild(MainGlobal.guildId);
+
             if (MainGlobal.Server != null)
             {
                 Console.WriteLine("| Server detected: " + MainGlobal.Server.Name);
             }
-
             else
             {
                 Console.WriteLine($"| A server has not yet been defined.");
@@ -20,9 +20,8 @@ namespace SallyBot
 
             while (MainGlobal.Server == null || MainGlobal.Server.Name == null || MainGlobal.Server.Name.Length < 1)
             {
-                var delayVar = Task.Delay(1200);
-                delayVar.Wait();
                 Console.WriteLine($"| Waiting for connection to be established by Discord...");
+                Task.Delay(1200).Wait();
             }
 
             Program.botUserId = MainGlobal.Client.CurrentUser.Id; // <--- bot's user ID is detected and filled in automatically
