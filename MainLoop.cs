1273dcc8a67fe9e8c72bff53a856fc46e5acaa9c
DeSinc <6deans@gmail.com>
2023-04-12 13:04:06 +1000

    fixed object ref not set to instance of object
    botName wasn't being grabbed successfully in MessageReceived. I forgot to remove this old line of code in the public build my bad.
    
    The BotName is now gotten via MainLoop.cs at login. It tries to use the bot's nickname if it has one, but gets the username instead if not.

--- MainLoop.cs
+++ MainLoop.cs
@@ -8,11 +8,11 @@ namespace SallyBot
         internal static Task StartLoop()
         {
             MainGlobal.Server = MainGlobal.Client.GetGuild(MainGlobal.guildId);
-
-            if (MainGlobal.Server != null)
+            if (MainGlobal.Server != null) // check if bot is in a server
             {
                 Console.WriteLine("| Server detected: " + MainGlobal.Server.Name);
             }
+
             else
             {
                 Console.WriteLine($"| A server has not yet been defined.");
@@ -20,11 +20,17 @@ namespace SallyBot
 
             while (MainGlobal.Server == null || MainGlobal.Server.Name == null || MainGlobal.Server.Name.Length < 1)
             {
+                var delayVar = Task.Delay(1200);
+                delayVar.Wait();
                 Console.WriteLine($"| Waiting for connection to be established by Discord...");
-                Task.Delay(1200).Wait();
             }
 
             Program.botUserId = MainGlobal.Client.CurrentUser.Id; // <--- bot's user ID is detected and filled in automatically
+
+            if (MainGlobal.Server.GetUser(Program.botUserId).Nickname != null) // check if there is a nickname to set
+            Program.botName = MainGlobal.Server.GetUser(Program.botUserId).Nickname;
+            else                                                               // otherwise, just use username if there is no nickname
+                Program.botName = MainGlobal.Server.GetUser(Program.botUserId).Username;
 
             return Task.CompletedTask;
         }
